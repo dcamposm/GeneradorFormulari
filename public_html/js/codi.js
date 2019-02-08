42e0934fdb8ac3a47d17495fe721d471535cc381
relleno <grupo.relleno@gmail.com>
2019-02-08 17:32:12 +0100

    Demetrio - Funciones Insert Input

--- public_html/js/codi.js
+++ public_html/js/codi.js
@@ -1,5 +1,23 @@
 $("#boto1").click(generadorFormulari);
-
+function insertInput(){
+    var inp = $(this).text();
+    //alert(inp);
+    
+    switch (inp){
+        case "Text":
+            $("#contForm").append("Formulari text: <input type=\"text\">");
+            break;
+        case "Numero":
+            $("#contForm").append("Formulari numero: <input type=\"number\">");
+            break;
+        case "Mail":
+            $("#contForm").append("Formulari correu: <input type=\"email\" value=\"Formulari correu\">");
+            break;
+        case "Password":
+            $("#contForm").append("Formulari Password: <input type=\"password\">");
+            break;
+    }
+};
 function generadorFormulari(){
    var container = document.createElement("div");
    $(container).attr("id", $('#form').val());
@@ -7,6 +25,9 @@ function generadorFormulari(){
    var titul = document.createElement("h1");
    var menu = document.createElement("div");
    $(menu).attr("class", "menu");
+   var contForm = document.createElement("div");
+   $(contForm).attr("id", "contForm");
+   $(contForm).attr("class", "contForm");
    
    /*---------------------MENU-----------------------*/
    $(menu).append("<h3>Menu</h3>");
@@ -14,11 +35,19 @@ function generadorFormulari(){
    $(menu).append("<button id=\"inputNum\">Numero</button>");
    $(menu).append("<button id=\"inputMail\">Mail</button>");
    $(menu).append("<button id=\"inputPass\">Password</button>");
-   /*-----------------------------------------------*/   
+   /*---------------------END------------------------*/   
    $(titul).append($('#form').val());
    
    $(container).append(titul);
+   $(container).append(contForm);
    $(container).append(menu);
-   $('main').append(container);
    
-}  
+   $('main').append(container);
+   /*-------------------EVENTOS----------------------*/ 
+   $("#inputText").click(insertInput); 
+   $("#inputNum").click(insertInput); 
+   $("#inputMail").click(insertInput); 
+   $("#inputPass").click(insertInput); 
+};  
+
+
