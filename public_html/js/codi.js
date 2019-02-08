1c837efa22f13397b5bb4e06818d1582d82f8711
relleno <grupo.relleno@gmail.com>
2019-02-08 17:02:37 +0100

    Demetrio - Base codigo js

--- public_html/js/codi.js
+++ public_html/js/codi.js
@@ -2,11 +2,23 @@ $("#boto1").click(generadorFormulari);
 
 function generadorFormulari(){
    var container = document.createElement("div");
-   var titul = document.createElement("h3");
-
+   $(container).attr("id", $('#form').val());
+   $(container).attr("class", "container");
+   var titul = document.createElement("h1");
+   var menu = document.createElement("div");
+   $(menu).attr("class", "menu");
+   
+   //-----------------MENU-----------------------
+   $(menu).append("<h3>Menu</h3>");
+   $(menu).append("<button id=\"inputText\">Text</button>");
+   $(menu).append("<button id=\"inputNum\">Numero</button>");
+   $(menu).append("<button id=\"inputMail\">Mail</button>");
+   $(menu).append("<button id=\"inputPass\">Password</button>");
+      
    $(titul).append($('#form').val());
-
+   
    $(container).append(titul);
+   $(container).append(menu);
    $('main').append(container);
    
 }  
